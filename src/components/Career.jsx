089f95c9bf74b8af463cad1748743b2f89e6e1f3
renoyoon <yhy8283@naver.com>
2025-04-04 17:17:03 +0900

    Revert "추가수정"
    This reverts commit 1e6b9612cf77d575df3ca0dfd2247c9a9f3c7a71.

--- src/components/Career.jsx
+++ src/components/Career.jsx
@@ -48,37 +48,30 @@ const Career = () => {
                     직급/직책
                 </div>
                 <div className="text">
-                    유컴패니온그룹<br></br>
-                    2023.01.16-2025.02.10<br></br>
-                    퍼블리셔 퍼블리싱팀 사원/팀원
+                    쇼메이커<br></br>
+                    2021.08.01-2022.09.02<br></br>
+                    웹퍼블리셔/웹디자인
                 </div>
             </div>
 
             <div className="con2">
+                <div className="title">IMAGE BANNER</div>
+                <div className="text">
+                    <span>주요 업무 :</span> 온라인 광고 콘텐츠 디자인 제작<br></br>
+                    <span>사용 Tool :</span> photoshop Illustrator<br></br>
+                    <span>상세 내용 :</span> - 마케팅 홍보 이미지 디자인 제작 | 기여도 100% 제작<br></br>
+                    다자인 기획. 콘텐츠 광고디자인 제작 SNS광고, 구글광고, 모비온광고, 네이버광고 콘텐츠 이미지 소재 제작
+                </div>
                 <div className="title">PUBLISHE</div>
                 <div className="text">
                     <span>주요 업무 :</span> 홈페이지 제작/수정<br></br>
-                    <span>사용 Tool :</span> <br></br>
-                    - 형상 관리 및 개발 환경: Git, GitHub, Bitbucket, Eclipse, VS Code<br></br>
-                    - 퍼블리싱 기술: HTML5, CSS3, SASS(SCSS), JavaScript(ES6+), jQuery<br></br>
-                    - 빌드 및 자동화 도구: Node.js, Gulp, Vite<br></br>
-                    - 협업 및 디자인 도구: Figma, Zeplin, Adobe XD<br></br>
-
-                    <span>상세 내용 :</span> <br></br>
-                    1. SKB 서비스 운영 및 UI/UX서비스 리뉴얼<br></br>
-                    - SK브로드밴드 주요 서비스(B tv, B box) 웹사이트 운영 및 유지보수<br></br>
-                    - 반응형 웹 및 웹 접근성(WCAG 2.1) 준수하여 UI/UX 개선<br></br>
-                    - SCSS(SASS) 및 CSS 모듈화를 통한 유지보수 효율성 향상<br></br>
-                    - Vite 및 Gulp를 활용한 빌드 자동화 및 최적화<br></br>
-                    - 크로스 브라우징 테스트(Chrome, Edge, Firefox, Safari 등) 및 디바이스별 최적화<br></br>
-                    - Eclipse 기반의 개발 환경에서 백엔드 연동 및 데이터 바인딩 최적화<br></br>
-                    - GitHub, Bitbucket을 활용한 형상 관리 및 코드 리뷰 진행<br></br>
-
-                    2. 디자이너 및 개발자와의 협업<br></br>
-                    - 디자이너(Figma, Zeplin, Adobe XD)와의 협업을 통해 디자인 가이드에 맞춘 UI 구현<br></br>
-                    - 디자인 시스템을 적용하여 일관된 UI 컴포넌트 구축 및 유지보수<br></br>
-                    - 반응형 웹 및 웹 접근성 최적화를 위해 디자이너와 협업하여 UI 개선<br></br>
-                    - GitHub, Bitbucket을 통한 코드 리뷰 및 브랜치 전략(Git Flow) 적용으로 협업 강
+                    <span>사용 Tool :</span> 카페24, bootstrap, vscode<br></br>
+                    <span>상세 내용 :</span> - 자사홈페이지 디자인 및 퍼블리싱 유지보수 | 기여도 100% 제작<br></br>
+                    기존 광고대행사 홈페이지 표준에 어긋나지 않게 웹 표준 및 웹 접근성 고려한 리뉴얼<br></br>
+                    새로운 Business/Solution/Service 웹 페이지 bootstrap활용, vscode 하드코딩 리뉴얼 퍼블리싱 제작<br></br>
+                    사이트 connect 홈페이지 email 전송 기능 추가<br></br>
+                    - 카페24기반 홈페이지 퍼블리싱, 웹 콘텐츠 팝업 배너 디자인 제작 | 기여도 100% 제작<br></br>
+                    쇼핑몰, 브랜드, 병원, 법률사무소 등 다양한 종류 홈페이지 퍼블리싱 수정/제작
                 </div>
             </div>
           </div>
